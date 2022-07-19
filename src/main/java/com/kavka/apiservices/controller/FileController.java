444d64f3663d503cc7f110338e4b9bbe3a00efb2
Sandesh Pokhrel <sandesh.pokhrel56@gmail.com>
2022-07-19 18:36:24 +0545

    log added

--- src/main/java/com/kavka/apiservices/controller/FileController.java
+++ src/main/java/com/kavka/apiservices/controller/FileController.java
@@ -20,6 +20,10 @@ public class FileController {
     private static final String PRODUCT_INSERT_QUERY = "INSERT INTO product (artist,code,color_wayfair,country_of_manufacture,description,feature_wayfair1,feature_wayfair2,feature_wayfair3,feature_wayfair4,feature_wayfair5,feature_wayfair6,holiday_wayfair,kavka_collection,lead_time_hours_wayfair,replacement_time_hours_wayfair,ship_type_wayfair,shopify_tags,name,product_category_id,image_url) VALUES (";
     private static final String PRODUCT_DETAIL_INSERT_QUERY = "INSERT INTO product_detail (code,sku,ct_sku,ct_print_template,carton_depth,carton_height,carton_width,other_image2,other_image3,other_image4,other_image5,other_image6,other_image7,other_image8,main_image_url,print_size,product_dimensions,product_max_depth,product_max_height,product_max_width,product_weight,other_image1,tier_one_price,tier_two_price,tier_three_price,product_type_wayfair,shipping_weight,price,product_id) VALUES (";
 
+    /**
+     * Provided the product Excel file it loads the data
+     * @param excel multipart excel file containing product data
+     */
     @PostMapping("/load/products/file")
     @ResponseStatus(HttpStatus.OK)
     public void loadProductFromFile(@RequestParam MultipartFile excel) throws IOException {
@@ -29,6 +33,10 @@ public class FileController {
         }
     }
 
+    /**
+     * Provided the product details Excel file it loads the data
+     * @param excel multipart excel file containing prodct details
+     */
     @PostMapping("/load/product-details/file")
     @ResponseStatus(HttpStatus.OK)
     public void loadProductDetailsFromFile(@RequestParam MultipartFile excel) throws IOException {
@@ -38,6 +46,10 @@ public class FileController {
         }
     }
 
+    /**
+     * Provided the path, it loads the products data
+     * @param path path to excel product file
+     */
     @GetMapping("/load/products")
     public void loadProducts(@RequestParam String path) throws IOException {
         FileInputStream file = new FileInputStream(path);
@@ -47,6 +59,10 @@ public class FileController {
         workbook.close();
     }
 
+    /**
+     * Provided the path, it loads the product details data
+     * @param path path to excel product details file
+     */
     @GetMapping("/load/product-details")
     public void loadProductDetails(@RequestParam String path) throws IOException {
         FileInputStream file = new FileInputStream(path);
